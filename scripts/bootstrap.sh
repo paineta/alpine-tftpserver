1dcba5f5e25a09f38d1425af07e3e8b4d8a05a3e
Thomas Paine <paineta@uwec.edu>
2019-10-24 09:32:17 -0500

    test for user, reduce startup noise

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,14 +5,19 @@ if [ ! -z "${TIMEZONE}" ] && [ -e "/usr/share/zoneinfo/${TIMEZONE}" ];then
    cp /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
    echo "${TIMEZONE}" > /etc/timezone
 fi
-adduser -s /sbin/nologin -DH -u ${TFTP_NOBODYID} nobody
+grep ^nobody /etc/passwd > /dev/null
+if [ "$?" != "0" ];then
+   adduser -s /sbin/nologin -DH -u ${TFTP_NOBODYID} nobody
+fi
 if [ -e "/remap" ];then
    TFTP_REMAP="-m /remap"
    echo -e "=== $(date) using map:\n$(cat /remap)\n==="
 fi
 if [ ! -z "${TFTP_OPTIONS}" ];then
-   echo -e "=== file structure\n$(ls -laR /tftproot)"
-   in.tftpd ${TFTP_OPTIONS} ${TFTP_EXTRAS} ${TFTP_REMAP} ${*} /tftproot
+   echo -e "=== tftproot\n$(ls -la /tftproot)\n"
+   cmd="in.tftpd ${TFTP_OPTIONS} ${TFTP_EXTRAS} ${TFTP_REMAP} ${*} /tftproot"
+   echo -e "=== daemon\n${cmd}\n"
+   ${cmd}
 else
    echo "need TFTP_OPTIONS"
    exit 1
